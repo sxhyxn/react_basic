9cc3e4831811efbf3cdb21bf136f1b721ba6566b
sxhyxn <129706893+sxhyxn@users.noreply.github.com>
2023-05-31 17:05:44 +0900

    Add files via upload

---
+++ #react2/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header'
+import Menu from './components/Menu';
+
+
+function App() {
+  return (
+    <div>
+      <Menu text="그린"/>
+      <Menu text="학원"/>
+      <Header color="빨강" num={1}/>
+      <Header color="파랑" num={2}/>
+      <Header color="녹색" num={3}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ #react2/src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Menu =({text}) => {
+    return (
+        <div>나는{text} 메뉴</div>
+    )
+ }
+
+
+ export default Menu;
